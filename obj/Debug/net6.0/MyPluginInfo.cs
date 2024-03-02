fb3cc75ed75c624c1114782b365c148eba644f91
supeshio <supeshio314@gmail.com>
2024-03-02 13:40:51 +0900

    up
    てすと

---
+++ obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TheSpaceRoles
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "TheSpaceRoles";
+        public const string PLUGIN_NAME = "TheSpaceRoles";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
