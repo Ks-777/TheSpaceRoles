58619272f13b510318a647f22541f735ef139d85
supeshio <supeshio314@gmail.com>
2024-03-09 23:00:00 +0900

    Add SheriffKillButton!
    Add SheriffKillButton and CustomButtons
    And Add RpcMurderPlayer

--- obj/Debug/net6.0/TheSpaceRoles.AssemblyInfo.cs
+++ obj/Debug/net6.0/TheSpaceRoles.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Many Funny Roles!")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbe797d8c73f28a5aa06d9f08107b95d122bacf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa281a2b6c08624c40564ae88856fcbe5ad74548")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheSpaceRoles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheSpaceRoles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
